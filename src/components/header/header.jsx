353bdb5babddcddcc2b2cbf445d8c3a5bda8597c
alaamagdy <alaamagdy@paymob.com>
2023-02-08 19:10:55 +0200

    finish header ui

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './header.css'
+import SearchIcon from '@mui/icons-material/Search';
 
 const PokHeader = () => {
   return (
     <div className='pokHeader'>
-      <h1>header</h1>
+          <div className='leftPart'>
+              <img className="pokImg" src='/src/imgs/pokemonIcon.png' alt=""/> 
+              <p className='pokText'>Pokemon Gallery</p>
+          </div>
+          <div className='rightPart'>
+              <span className='searchSpan'>
+              <SearchIcon/>
+              </span>
+              <input className='searchInput' placeholder='Search'/>
+          </div>
       </div>
   )
 }
